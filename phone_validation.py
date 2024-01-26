2602ffb594eec5ff3d1af4e12ca74bddcbd86ae4
lucasNSF <nascimentomr10@gmail.com>
2024-01-25 23:29:25 -0300

    refactor: change phone_validation regex

--- phone_validation.py
+++ phone_validation.py
@@ -1,7 +1,7 @@
 import re
 
 def phone_validation(phone):
-  regex = r'^(\d{2})?[-. ]?(\d{8})$'
+  regex = r'^(\+55\s?)?(\(\d{2}\)\s?|\d{2}\s?)(9\d{4}|\d{4})-\d{4}$'
   if re.match(regex, phone):
     return True
   return False
